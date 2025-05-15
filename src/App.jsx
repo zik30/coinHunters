b6fa0c2f1e56b3abc4241e95d734f77eee244761
zik30 <ainazik.ishenbek@gmail.com>
2025-05-16 01:50:05 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<div>Hello!</div>}/>
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
+)
