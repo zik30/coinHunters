d8146284b9d663c4829740586d45e3e83aecf97d
zik30 <ainazik.ishenbek@gmail.com>
2025-05-17 01:57:42 +0600

    clean vite game

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import GamePage from './pages/gamePage/GamePage'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<div>Hello!</div>}/>
+      <Route path='/game' element={<GamePage/>}/>
     </Routes>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
