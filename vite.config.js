ace3986c243314d4d32499be8b328fbcc5a82bec
zik30 <ainazik.ishenbek@gmail.com>
2025-05-20 13:32:45 +0600

    default styles

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@app/styles/forward.scss" as *;
+        `,
+      },
+    },
+  },
 })
