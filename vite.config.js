00b12c9a039352e585de71cfa8bd8c98bd4b061f
zik30 <ainazik.ishenbek@gmail.com>
2025-05-21 10:53:39 +0600

    fixes in global styles

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@pages': '/src/pages',
+      '@styles': '/src/styles',
+      '@components': '/src/components',
+      '@assets': '/src/assets',
+      '@api': '/src/api',
+      '@store': '/src/store'
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `
-          @use "@app/styles/forward.scss" as *;
-        `,
+        additionalData: `@use "@styles/forward.scss";`,
       },
     },
   },
